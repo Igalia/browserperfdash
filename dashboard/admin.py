46f738675d2856eec3909ec5ce66d96f8b3841fd
Tony Thomas <01tonythomas@gmail.com>
2016-12-07 12:46:20 +0100

    Added in CPU, GPU and platform models. Added them to admin pages too

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from dashboard.models import CPUArchitecture, GPUType, Platform
 
 # Register your models here.
+
+
+class CPUArchitectureAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled')
+admin.site.register(CPUArchitecture, CPUArchitectureAdmin)
+
+
+class GPUTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled')
+admin.site.register(GPUType, GPUTypeAdmin)
+
+
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled')
+admin.site.register(Platform, PlatformAdmin)
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,5 +1,32 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+class CPUArchitecture(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(_('CPU Architecture Name'), max_length=50, blank=False, unique=True)
+    enabled = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class GPUType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(_('GPU Type Name'), max_length=50, blank=False, unique=True)
+    enabled = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Platform(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(_('Platform Name'), max_length=50, blank=False, unique=True)
+    enabled = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.name
