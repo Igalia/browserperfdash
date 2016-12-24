96bec1610f6a0b434f088b4450d1818aacc46032
Tony Thomas <01tonythomas@gmail.com>
2016-12-25 00:39:13 +0100

    Use ng-resource to load in all the results

--- dashboard/static/js/dashboard.module.js
+++ dashboard/static/js/dashboard.module.js
@@ -1,11 +1,15 @@
-app = angular.module('browserperfdash.dashboard.static', []);
+app = angular.module('browserperfdash.dashboard.static', ['ngResource']);
 
-app.controller('AppController', function($scope, $http) {
-    $scope.reports = [];
-    $http.get('/dash/results')
-        .then(function(result) {
-            angular.forEach(result.data, function (report) {
-                $scope.reports.push(report);
-            })
-        });
+app .factory('botReportsFactory', function($resource) {
+  return $resource('/dash/results');
+});
+
+app.controller('AppController', function($scope, $http, botReportsFactory) {
+    $scope.reports = botReportsFactory.query();
+    // $http.get('/dash/results')
+    //     .then(function(result) {
+    //         angular.forEach(result.data, function (report) {
+    //             $scope.reports.push(report);
+    //         })
+    //     });
 });
