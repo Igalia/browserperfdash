f3a879f28a17745db246e7c332cc96b262f1a855
Tony Thomas <01tonythomas@gmail.com>
2016-12-27 00:33:03 +0100

    Add filtering by age of report

--- dashboard/serializers.py
+++ dashboard/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import BotReportData, Browser, Bot, Platform, GPUType, CPUArchitecture
-
+import datetime
+import pytz
 
 # Serializers define the API representation.
 class BrowserListSerializer(serializers.ModelSerializer):
@@ -38,10 +39,15 @@ class BotReportDataSerializer(serializers.ModelSerializer):
     gpu_type = serializers.CharField(source='bot.gpuType', read_only=True)
     cpu_arch = serializers.CharField(source='bot.cpuArchitecture', read_only=True)
     platform = serializers.CharField(source='bot.platform', read_only=True)
+    days_since = serializers.SerializerMethodField()
+
+    def get_days_since(self,obj):
+        current_time = pytz.utc.localize(datetime.datetime.utcnow())
+        return (current_time - obj.timestamp).days
 
     class Meta:
         model = BotReportData
         fields = ('bot', 'gpu_type', 'cpu_arch', 'platform', 'browser', 'browser_version', 'root_test',
-                  'test_path', 'test_version', 'metric_tested', 'mean_value', 'stddev', 'timestamp')
+                  'test_path', 'test_version', 'metric_tested', 'mean_value', 'stddev', 'days_since')
 
 
