95cd0f7543795b27eb5cafa2d2a58f21c8a9ca40
Tony Thomas <01tonythomas@gmail.com>
2016-12-27 00:35:43 +0100

    Add manual refresh, with button click

--- dashboard/static/js/dashboard.module.js
+++ dashboard/static/js/dashboard.module.js
@@ -36,5 +36,4 @@ app.controller('AppController', function($scope, botReportsFactory, browserFacto
         $scope.reports = botReportsFactory.query();
     };
     $scope.reload();
-    $interval($scope.reload, 50000);
 });
