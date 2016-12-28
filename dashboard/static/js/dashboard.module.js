6bfb699f1a87ac9f0928dbd07e250b4d0eca1990
Tony Thomas <01tonythomas@gmail.com>
2016-12-28 15:02:58 +0100

    Make sure all combo boxes.value is set according to bot selection

--- dashboard/static/js/dashboard.module.js
+++ dashboard/static/js/dashboard.module.js
@@ -32,6 +32,13 @@ app.controller('AppController', function($scope, botReportsFactory, browserFacto
     $scope.platforms = platformFactory.query();
     $scope.gpus = gpuFactory.query();
     $scope.cpus = cpuArchFactory.query();
+    $scope.updateOtherCombos = function () {
+        if ( $scope.selectedBot ) {
+            $scope.selectedPlatform = $scope.selectedBot.platform;
+            $scope.selectedCPU = $scope.selectedBot.cpu;
+            $scope.selectedGPU = $scope.selectedBot.gpu;
+        }
+    };
     $scope.reload = function () {
         $scope.reports = botReportsFactory.query();
     };
