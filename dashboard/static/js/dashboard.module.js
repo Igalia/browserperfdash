98980f8946f474bb0dfae070f901a7676ec5e83e
Tony Thomas <01tonythomas@gmail.com>
2017-03-07 22:15:01 +0100

    Add in pan and zoom to chart

--- dashboard/static/js/dashboard.module.js
+++ dashboard/static/js/dashboard.module.js
@@ -256,7 +256,27 @@ app.controller('PlotController', function ($scope, browserForResultExistFactory,
                         ];
                     }
                 }
+            },
+            // Container for pan options
+            pan: {
+                // Boolean to enable panning
+                enabled: true,
+
+                // Panning directions. Remove the appropriate direction to disable
+                // Eg. 'y' would only allow panning in the y direction
+                mode: 'xy'
+            },
+
+            // Container for zoom options
+            zoom: {
+                // Boolean to enable zooming
+                enabled: true,
+
+                // Zooming directions. Remove the appropriate direction to disable
+                // Eg. 'y' would only allow zooming in the y direction
+                mode: 'xy',
             }
+
         };
     };
 
