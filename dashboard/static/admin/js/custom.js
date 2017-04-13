9abd8a5bca3d19cfe569fb996c98e4ef83557a27
Tony Thomas <01tonythomas@gmail.com>
2017-04-13 15:12:08 +0200

    [#5] Use Django-JQuery to populate in metrics fields

---
+++ dashboard/static/admin/js/custom.js
@@ -0,0 +1,38 @@
+(function($) {
+    $('#addrowlink').click(function () {
+        var metricsTable = document.getElementById("metrics_table");
+        var currentIndex = metricsTable.rows.length;
+        var currentRow = metricsTable.insertRow(-1);
+
+        var keyBox = document.createElement("input");
+        keyBox.setAttribute("name", "key_" + currentIndex);
+        keyBox.setAttribute("class", "vTextField");
+
+        var valueBox = document.createElement("input");
+        valueBox.setAttribute("name", "value_" + currentIndex);
+        valueBox.setAttribute("class", "vTextField");
+        valueBox.setAttribute("type", "number");
+
+        var removeLinkContainer = document.createElement("p");
+        removeLinkContainer.setAttribute("class", "deletelink-box");
+
+        var removeLink = document.createElement("a");
+        removeLink.setAttribute("class","deletelink");
+        removeLink.setAttribute("onclick", 'removeRow(this)');
+
+        var removeText = document.createTextNode("remove");
+        removeLink.appendChild(removeText);
+        removeLinkContainer.appendChild(removeLink);
+
+        var currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(keyBox);
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(valueBox);
+        currentCell = currentRow.insertCell(-1);
+        currentCell.appendChild(removeLinkContainer);
+    });
+    $('.deletelink').click(function () {
+        $(this).closest("tr").remove();
+    })
+
+})(django.jQuery);
