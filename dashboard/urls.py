91a88c4d885cd897a2c084635d23f5c155a0810d
Carlos Alberto Lopez Perez <clopez@igalia.com>
2024-07-22 21:55:19 +0100

    Fix urls regex: a test name can have a dot, like in motionmark1.3

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -26,14 +26,14 @@ urlpatterns = [
     url(r'^bot_results_exist/(?P<browser>.+)$', BotsForResultsExistList.as_view()),
     url(r'^bot_full_details_for_exist/(?P<browser>.+)$', BotsFullDetailsForResultsExistList.as_view()),
     url(r'^testpath/(?P<browser>.+)/(?P<test>.+)$', TestPathList.as_view()),
-    url(r'^test_metrics/(?P<test>[-\w]+)/(?P<subtest>.+)$', MetricsForTestList.as_view()),
+    url(r'^test_metrics/(?P<test>[-\w\.]+)/(?P<subtest>.+)$', MetricsForTestList.as_view()),
     url(r'^tests_for_browser_bot/(?P<browser>.+)/(?P<bot>.*)$', TestsForBrowserBotList.as_view()),
-    url(r'^results_for_subtest/(?P<browser>[-\w]+)/(?P<test>[-\w]+)/(?P<bot>[-\w]+)/(?P<subtest>.+)/$',
+    url(r'^results_for_subtest/(?P<browser>[-\w]+)/(?P<test>[-\w\.]+)/(?P<bot>[-\w]+)/(?P<subtest>.+)/$',
         ResultsForSubtestList.as_view()),
     url(r'^report/improvement/(?P<days_since>\d+)/(?P<platform>[-\w]+)/(?P<gpu>[-\w]+)/'
-        r'(?P<cpu>[-\w]+)/(?P<browser>[-\w]+)/(?P<test>[-\w]+)/(?P<bot>[-\w]+)/(?P<limit>\d+)/$',
+        r'(?P<cpu>[-\w]+)/(?P<browser>[-\w]+)/(?P<test>[-\w\.]+)/(?P<bot>[-\w]+)/(?P<limit>\d+)/$',
         BotDataReportImprovementListView.as_view()),
     url(r'^report/regression/(?P<days_since>\d+)/(?P<platform>[-\w]+)/(?P<gpu>[-\w]+)/'
-        r'(?P<cpu>[-\w]+)/(?P<browser>[-\w]+)/(?P<test>[-\w]+)/(?P<bot>[-\w]+)/(?P<limit>\d+)/$',
+        r'(?P<cpu>[-\w]+)/(?P<browser>[-\w]+)/(?P<test>[-\w\.]+)/(?P<bot>[-\w]+)/(?P<limit>\d+)/$',
         BotDataReportRegressionListView.as_view())
 ]
